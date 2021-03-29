9c87b95e42b8fc659111d26ef6a1ea2949805602
Kevin Adam <kevinadam@gmx.de>
2021-03-29 19:33:02 +0200

    adjusted social media meta tags

--- src/components/seo/SEO.jsx
+++ src/components/seo/SEO.jsx
@@ -17,31 +17,26 @@ const SEO = () => {
   const socialImageUrl = `${siteUrl}${src}`;
 
   return (
-    <Helmet
-      title={title}
-      titleTemplate={titleTemplate}
-      htmlAttributes={{
-        lang,
-      }}
-    >
+    <Helmet title={title} titleTemplate={titleTemplate}>
+      {/* General Tags */}
       <html lang={lang} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" content="text/html" />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords.join(",")} />
+      <meta name="image" content={socialImageUrl} />
 
-      {/** OpenGraph aka Facebook Tags */}
+      {/* OpenGraph aka Facebook Tags */}
       <meta name="og:title" content={title} />
       <meta name="og:site_name" content={title} />
       <meta name="og:description" content={description} />
       <meta name="og:url" content={siteUrl} />
+      <meta name="og:image" content={socialImageUrl} />
       <meta name="og:image:height" content={height} />
       <meta name="og:image:width" content={width} />
-      <meta name="og:image" content={socialImageUrl} />
-      <meta name="og:image:secure" content={socialImageUrl} />
       <meta name="og:type" content="website" />
 
-      {/** Twitter Tags */}
+      {/* Twitter Tags */}
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={socialImageUrl} />
