748502436b41428f1f917eaa658fc9c0713ef942
Kevin Adam <kevinadam@gmx.de>
2021-03-29 18:03:01 +0200

    fixed warning for missing import

--- src/components/cv/Experience.jsx
+++ src/components/cv/Experience.jsx
@@ -1,4 +1,4 @@
-import { useStaticQuery } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import React from "react";
 import { Element } from "react-scroll";
 import experience from "../../content/experience.json";
